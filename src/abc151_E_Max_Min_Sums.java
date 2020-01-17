8489ba12e6dc5eb0d61edd5b2ca43bf12d21d2a8
KetaoChen <kchen343@gatech.edu>
2020-01-16 22:03:52 -0500

    inv array

--- src/abc151_E_Max_Min_Sums.java
+++ src/abc151_E_Max_Min_Sums.java
@@ -23,16 +23,31 @@ public class abc151_E_Max_Min_Sums {
         int mod = 1000000007;
         int l = arr.length;
         long base = 1;
-
+        long[] inv = invArray(l - m + 1, mod);
         for (int i = 0; i <= l - m; i++) {
             //System.out.println(arr[m + i - 1] + " " + arr[l - m - i] + " " + base);
             sum = (sum + base * arr[m + i - 1] - base * arr[l - m - i]) % mod;
-            while (base % (i + 1) != 0) {
-                base += mod;
-            }
-            base = (base * (m + i) / (i + 1)) % mod;
+            base = (base * (m + i) % mod) * (inv[i + 1] % mod) % mod;
         }
 
         return (int) ((sum + mod) % mod);
+    }
+
+    public  static long[] invArray(int n, int mod){
+        long[] inv = new long[n + 1];
+        inv[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            inv[i] = (mod - mod / i) * inv[mod % i] % mod;
+        }
+
+        return inv;
+    }
+
+    private static long[] getInverseMod(int i, int mod) {
+        if (mod == 0) {
+            return new long[]{1, 0};
+        }
+        long[] temp = getInverseMod(mod, i  % mod);
+        return new long[]{temp[1], temp[0] - i / mod * temp[1]};
     }
 }
